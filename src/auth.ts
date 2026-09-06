aeb8a3a02b3bd3c84a60162c32398a8531010844
Shazia757 <shaziashaz226@gmail.com>
2026-09-06 10:45:19 +0530

    authenticated document storage

--- src/auth.ts
+++ src/auth.ts
@@ -52,8 +52,29 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     }),
   ],
 
+  callbacks: {
+    ...authConfig.callbacks,
+
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+
+      return token;
+    },
+
+    async session({ session, token }) {
+      if (session.user && token.id) {
+        session.user.id = token.id as string;
+      }
+
+      return session;
+    },
+  },
+
   session: {
     strategy: "jwt",
   },
+
 
 });
